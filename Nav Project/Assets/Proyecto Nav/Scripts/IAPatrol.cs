8614c637aed7a6933fd27d5acdcee468a49afe03
Pincho <pinchodev91@gmail.com>
2017-04-27 11:27:42 +0200

    Implementado Enemigos

---
+++ Nav Project/Assets/Proyecto Nav/Scripts/IAPatrol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IAPatrol : MonoBehaviour {
+
+	public NavMeshAgent navegador;
+
+	public Transform[] patrolPoint;
+
+	public int currentPatrolPoint;
+
+	private void Start()
+	{
+		// Asignamos los componentes
+		navegador = GetComponent<NavMeshAgent>();
+
+		// Inicializamos la patrulla
+		currentPatrolPoint = -1;
+		GoToNextPatrolPoint();
+	}
+
+	private void GoToNextPatrolPoint()
+	{
+		currentPatrolPoint++;
+
+		if(currentPatrolPoint >= patrolPoint.Length) currentPatrolPoint = 0;
+
+		navegador.SetDestination(patrolPoint[currentPatrolPoint].position);
+	}
+}
