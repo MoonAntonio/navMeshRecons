f8b75843fe7dff4f81d7584ae4ba4ba9e75497b1
Pincho <pinchodev91@gmail.com>
2017-04-27 09:42:01 +0200

    Add PlayerController.cs

---
+++ Nav Project/Assets/Proyecto Nav/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace MoonAntonio
+{
+	public class PlayerController : MonoBehaviour 
+	{
+		// Componentes
+		public NavMeshAgent navegador;
+
+		private void Start()
+		{
+			navegador = GetComponent<NavMeshAgent>();
+
+			// SetDestination ir al punto dado
+			// NavMeshAgent precalcula la ruta y la ejecuta a lo largo del tiempo
+			navegador.SetDestination(new Vector3(3.4f,0.24f,-10.27f));
+		}
+	}
+}
